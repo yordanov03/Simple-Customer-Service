c519351ae14eb2844d28a1645a8edfb1714e4b22
Svetoslav.Yordanov <shapeshifter003@gmail.com>
2020-04-14 21:44:58 +0300

    api ready

--- SimpleCustomerService/SimpleCustomerService.Web/Startup.cs
+++ SimpleCustomerService/SimpleCustomerService.Web/Startup.cs
@@ -2,9 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SimpleCustomerService.Data;
+using SimpleCustomerService.Services;
+using SimpleCustomerService.Services.interfaces;
 
 namespace SimpleCustomerService.Web
 {
@@ -17,9 +21,18 @@ namespace SimpleCustomerService.Web
 
         public IConfiguration Configuration { get; }
 
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SimpleCustomerServiceDbContext>(options => {
+                options.UseSqlite(Configuration.GetConnectionString("CustomersSqliteConnectionString"));
+            });
+
+            services.AddScoped<ICustomerService, CustomerService>();
+            services.AddScoped<IStatesService, StatesService>();
+            services.AddTransient<CustomerDbSeeder>();
+
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -29,7 +42,7 @@ namespace SimpleCustomerService.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CustomerDbSeeder customerDbSeeder)
         {
             if (env.IsDevelopment())
             {
@@ -70,6 +83,7 @@ namespace SimpleCustomerService.Web
                     spa.UseAngularCliServer(npmScript: "start");
                 }
             });
+            customerDbSeeder.SeedAsync(app.ApplicationServices).Wait();
         }
     }
 }
