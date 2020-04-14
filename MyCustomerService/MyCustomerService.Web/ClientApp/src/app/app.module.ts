6869d8f35a271b32ff258fc8ded79a1a8f7af9ba
Svetoslav.Yordanov <shapeshifter003@gmail.com>
2020-04-14 14:30:23 +0300

    added customers component

--- MyCustomerService/MyCustomerService.Web/ClientApp/src/app/app.module.ts
+++ MyCustomerService/MyCustomerService.Web/ClientApp/src/app/app.module.ts
@@ -7,22 +7,26 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { CustomersComponent } from './customers/customers/customers.component';
+import { CustomersGridComponent } from './customers/grid/customers-grid/customers-grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-
+    FetchDataComponent,
+    CustomersComponent,
+    CustomersGridComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      
-      { path: '', component: HomeComponent, pathMatch: 'full' }
-      
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
   providers: [],
